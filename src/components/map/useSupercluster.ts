540c086461d1ec0d87605b8649ef2faabcb3199b
James Dinsdale <hi@molovo.co>
2022-03-24 16:33:45 +0000

    Add new map component

---
+++ src/components/map/useSupercluster.ts
@@ -0,0 +1,49 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+import Supercluster, { AnyProps, PointFeature } from 'supercluster'
+import { Bounds, MarkerProps } from './types'
+
+const useSupercluster = (
+  markers: MarkerProps[] = [],
+  bounds: Bounds,
+  zoom: number = 10,
+  radius: number = 25,
+) => {
+  const supercluster = useRef<Supercluster>(null)
+  const [clusters, setClusters] = useState([])
+
+  const formatGeopoint = useCallback(
+    item =>
+      ({
+        type: 'Feature',
+        properties: {
+          cluster: false,
+          store: item,
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [item.center.lng, item.center.lat],
+        },
+      } as PointFeature<AnyProps>),
+    [],
+  )
+
+  useEffect(() => {
+    if (markers.length === 0) {
+      supercluster.current = null
+      setClusters([])
+    } else {
+      supercluster.current = new Supercluster({
+        radius,
+        maxZoom: 20,
+      })
+      supercluster.current.load(markers.map(formatGeopoint))
+      if (bounds && zoom) {
+        setClusters(supercluster.current.getClusters(bounds, zoom))
+      }
+    }
+  }, [markers, formatGeopoint, setClusters, bounds, zoom])
+
+  return { clusters, supercluster: supercluster.current }
+}
+
+export default useSupercluster
