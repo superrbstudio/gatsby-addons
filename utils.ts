70a43fff6fe930c82551becf881a70517fc8e747
James Dinsdale <hi@molovo.co>
2022-04-06 10:24:19 +0100

    Add animate helper and animator

--- utils.ts
+++ utils.ts
@@ -1,3 +1,6 @@
 import extendClass from './src/utils/extend-class'
+import * as Easing from './src/utils/easing-functions'
+import animate from './src/utils/animate'
+import { animator } from './src/utils/animator'
 
-export { extendClass }
+export { extendClass, animator, animate, Easing }
