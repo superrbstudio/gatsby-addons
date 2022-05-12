70f35402824701daea88913e0b6a43f3b9040f16
James Dinsdale <hi@molovo.co>
2022-05-12 12:31:56 +0100

    Include storage factory directly in repository due to incorrect export in plugin

--- storage.ts
+++ storage.ts
@@ -1,4 +1,4 @@
-import { storageFactory } from 'storage-factory'
+import { storageFactory } from './src/utils/storage-factory'
 
 export const local = storageFactory(() => localStorage)
 export const session = storageFactory(() => sessionStorage)
