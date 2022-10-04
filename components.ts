ed404f26f33c622e84fff3a1734cef81d8c1881e
James Dinsdale <hi@molovo.co>
2022-10-04 12:20:10 +0100

    Export structured data components

--- components.ts
+++ components.ts
@@ -6,5 +6,6 @@ import Image from './src/components/image'
 import Map from './src/components/map/map'
 import MenuToggle from './src/components/menu-toggle'
 import Seo from './src/components/seo'
+import * as StructuredData from './src/components/structured-data'
 
-export { Button, Image, Map, MenuToggle, Seo }
+export { Button, Image, Map, MenuToggle, Seo, StructuredData }
