3ed1abdaa888adb49f17f9427787b8e5c4f89557
James Dinsdale <hi@molovo.co>
2022-10-22 22:00:53 +0100

    Add useEventListener hook

---
+++ src/hooks/use-event-listener.ts
@@ -0,0 +1,59 @@
+import { MutableRefObject, useEffect, useRef } from 'react'
+
+type EventName = keyof GlobalEventHandlersEventMap
+
+type EventListener<E extends EventName> = (
+  event: GlobalEventHandlersEventMap[E]
+) => void | boolean
+
+type Target = Document | Window | Element
+
+// Hook
+const useEventListener = <E extends EventName>(
+  eventName: E,
+  handler: EventListener<E>,
+  options: boolean | AddEventListenerOptions = {},
+  element: Target = window
+) => {
+  // Create a ref that stores handler
+  const savedHandler = useRef() as MutableRefObject<EventListener<E>>
+
+  // Update ref.current value if handler changes.
+  // This allows our effect below to always get latest handler ...
+  // ... without us needing to pass it in effect deps array ...
+  // ... and potentially cause effect to re-run every render.
+  useEffect(() => {
+    savedHandler.current = handler
+  }, [handler])
+
+  useEffect(
+    () => {
+      // Make sure element supports addEventListener
+      // On
+      const isSupported = element && element.addEventListener
+      if (!isSupported) return
+
+      // Create event listener that calls handler function stored in ref
+      const eventListener = (event: GlobalEventHandlersEventMap[E]) =>
+        savedHandler.current(event)
+
+      // Add event listener
+      element.addEventListener(
+        eventName,
+        eventListener as EventListenerOrEventListenerObject,
+        options
+      )
+
+      // Remove event listener on cleanup
+      return () => {
+        element.removeEventListener(
+          eventName,
+          eventListener as EventListenerOrEventListenerObject
+        )
+      }
+    },
+    [eventName, element] // Re-run if eventName or element changes
+  )
+}
+
+export default useEventListener
