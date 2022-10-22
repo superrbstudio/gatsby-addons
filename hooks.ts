f6424143296637c0b755fc495cebb4d26991bd8d
James Dinsdale <hi@molovo.co>
2022-10-22 22:00:53 +0100

    Add useEventListener hook

--- hooks.ts
+++ hooks.ts
@@ -2,6 +2,7 @@
  * Hooks
  */
 import useAsync from './src/hooks/use-async'
+import useEventListener from './src/hooks/use-event-listener'
 import useId from './src/hooks/use-id'
 import useIsInViewport from './src/hooks/use-is-in-viewport'
 import useIsMobile from './src/hooks/use-is-mobile'
@@ -10,6 +11,7 @@ import useMotionAllowed from './src/hooks/use-motion-allowed'
 
 export {
   useAsync,
+  useEventListener,
   useId,
   useIsInViewport,
   useIsMobile,
