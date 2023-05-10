1d88f2b4813072c514204a15ca20a24fa397f137
James Dinsdale <hi@molovo.co>
2023-05-10 14:12:47 +0100

    Include link resolver in link component

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { HTMLAttributes } from 'react'
 import { Link as GatsbyLink } from 'gatsby'
+import { Link as LinkType } from '../../types'
 import isExternalLink from '../utils/is-external-link'
+import linkResolver from 'ProjectRoot/src/utils/linkResolver'
 
-const Link = ({ to, ...props }) => {
+interface Props extends HTMLAttributes<HTMLAnchorElement> {
+  to: LinkType | string | undefined
+}
+
+const Link = ({ to, ...props }: Props) => {
+  if ('type' in to || 'url' in to) {
+    to = linkResolver(to)
+  }
+
   if (isExternalLink(to)) {
     return <a href={to} {...props} />
   }
