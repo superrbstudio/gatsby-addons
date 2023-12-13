12d8f55701a56b9a459c5fc18afd235d9b474d02
Giles Smith <giles@superrb.com>
2023-12-13 12:57:42 +0000

    feat: allow image domain override for use with reverse proxy

--- src/hooks/use-image-sources.ts
+++ src/hooks/use-image-sources.ts
@@ -1,9 +1,23 @@
 import { Image } from '../../types'
+import { SourceProps } from "gatsby-plugin-image/dist/src/components/picture";
 
 const useImageSources = (image: Image) => {
   // Alias images from files
   if (image?.file?.childImageSharp?.gatsbyImageData) {
     image.gatsbyImageData = image.file.childImageSharp.gatsbyImageData
+  }
+
+  if (image?.gatsbyImageData?.images?.fallback?.src && image?.gatsbyImageData?.images?.sources && process.env.GATSBY_IMAGE_URL_FIND && process.env.GATSBY_IMAGE_URL_REPLACE) {
+    image.gatsbyImageData.images.fallback.src = image.gatsbyImageData.images.fallback.src.replace(process.env.GATSBY_IMAGE_URL_FIND, process.env.GATSBY_IMAGE_URL_REPLACE)
+    let newSources: { sizes: string | undefined; srcSet: string | undefined; type: string | undefined }[] = []
+    image.gatsbyImageData.images.sources.forEach(function (item: SourceProps) {
+      newSources.push({
+        sizes: item.sizes,
+        srcSet: item.srcSet.replace(process.env.GATSBY_IMAGE_URL_FIND as string, process.env.GATSBY_IMAGE_URL_REPLACE as string),
+        type: item.type,
+      })
+    })
+    image.gatsbyImageData.images.sources = newSources
   }
 
   return {
